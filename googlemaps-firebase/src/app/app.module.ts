9102d3cbf46b0a864bc7924f0866f301096a2295
Ahkalamiammiam <vianneytraina@gmail.com>
2019-09-20 18:32:07 +0200

    Add a New Donor Data using Angular Material

--- googlemaps-firebase/src/app/app.module.ts
+++ googlemaps-firebase/src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppComponent } from './app.component';
 import { DonorListComponent } from './donor-list/donor-list.component';
 import { AddDonorComponent } from './add-donor/add-donor.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 const appRoutes: Routes = [
   { path: 'donor-list', component: DonorListComponent },
@@ -24,9 +37,22 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
     RouterModule.forRoot(
       appRoutes
-    )
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
